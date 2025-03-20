008f5a5eec63d935443b04907926b56d7fa714b9
Dylan Groux <dylangroux2105@gmail.com>
2025-03-20 14:52:11 +0100

    Tache 5 : Add category management features: create category form, update admin templates, and enhance playlist repository

---
+++ src/Form/CategorieType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use App\Entity\Playlist;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la catégorie *',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => $options['is_edit'] ? 'Modifier' : 'Créer',
+                'attr' => [
+                    'class' => 'btn btn-primary mt-3',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+            'is_edit' => false,
+        ]);
+    }
+}
